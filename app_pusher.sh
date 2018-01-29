932ac7121a68aea54635bf8156337ede69f00feb
Travis Mattera <haggishunk@hotmail.com>
2018-01-28 16:24:19 -0800

    Added option to pass an arbitrary application git source

--- app_pusher.sh
+++ app_pusher.sh
@@ -2,16 +2,20 @@
 
 echo "*** APP_PUSHER ***"
 
-if [ -d ruby-rails-sample ];
-then 
-    cd ruby-rails-sample;
-    git remote remove dokku;
-    cd ..;
+if [ -d $3 ];
+then
+    echo "You already have a local copy of the application: $3";
+    # cd ruby-rails-sample;
+    # git remote remove dokku;
+    # cd ..;
+    cd $3;
 else 
-    git clone git@github.com:heroku/ruby-rails-sample.git;
+    echo "Downloading application: $3";
+    git clone "git@github.com:$3.git";
+    cd $3;
+    echo "Adding dokku git remote: dokku@$1:$2";
+    git remote add dokku dokku@$1:$2;
 fi;
 
-cd ruby-rails-sample;
-git remote add dokku dokku@$1:$2;
 
 GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git push dokku master;

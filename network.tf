4eef558d292264c142eabc38b3c63c424705a226
Travis Mattera <haggishunk@hotmail.com>
2018-01-29 08:13:10 -0800

    Fixed floating ip droplet assignment

--- network.tf
+++ network.tf
@@ -1,22 +1,26 @@
 # requisition a floating IP for each paas node
 resource "digitalocean_floating_ip" "mypaas" {
   count      = "${var.instances}"
-  droplet_id = "${element(digitalocean_droplet.mypaas.*.ipv4_address, count.index)}"
+  droplet_id = "${element(digitalocean_droplet.mypaas.*.id, count.index)}"
   region     = "${var.region}"
 }
 
 # set up the base domain A record for your PaaS
 # defaults to first node
-resource "digitalocean_domain" "mypaas" {
-  name       = "${var.prefix}.${var.domain}"
-  ip_address = "${digitalocean_floating_ip.mypaas.0.ip_address}"
-}
+# resource "digitalocean_domain" "mypaas" {
+#   name       = "${var.prefix}.${var.domain}"
+#   ip_address = "${digitalocean_floating_ip.mypaas.0.ip_address}"
+# }
 
-# add a wildcard CNAME record to support appname subdomains
-resource "digitalocean_record" "mypaas-wildcard" {
-  domain = "${digitalocean_domain.mypaas.id}"
-  type   = "CNAME"
-  name   = "wildcard"
-  value  = "*.${digitalocean_domain.mypaas.id}"
-  ttl    = 600
-}
+# # add a wildcard CNAME record to support appname subdomains
+# resource "digitalocean_record" "mypaas-wildcard" {
+#   domain = "${digitalocean_domain.mypaas.id}"
+#   type   = "CNAME"
+#   name   = "wildcard"
+#   value  = "*.${digitalocean_domain.mypaas.id}"
+#   ttl    = 600
+# }
+
+# output "domain" {
+#   value = "${digitalocean_domain.mypaas.id}"
+# }
